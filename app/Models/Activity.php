934598e9f6ff60fb6a7039c4c97fcbcaaa600cec
Luigi Gandemer <luigigandemer6@gmail.com>
2025-10-27 18:54:37 +0100

    Activity model

---
+++ app/Models/Activity.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Activity extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['workspace_id', 'user_id', 'type', 'description'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function workspace()
+    {
+        return $this->belongsTo(Workspace::class);
+    }
+
+    public function subject()
+    {
+        return $this->morphTo();
+    }
+}

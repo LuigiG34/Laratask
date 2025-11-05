85577515192560e297ae29e543bb701340f9c6d5
Luigi Gandemer <luigigandemer6@gmail.com>
2025-11-05 13:10:32 +0100

    Update the navs

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,14 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+
+                    <x-nav-link :href="route('projects.index')" :active="request()->routeIs('projects.*')">
+                        {{ __('Projects') }}
+                    </x-nav-link>
+
+                    <x-nav-link :href="route('my-tasks')" :active="request()->routeIs('my-tasks')">
+                        {{ __('My Tasks') }}
+                    </x-nav-link>
                 </div>
             </div>
 
@@ -101,6 +109,14 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+
+            <x-responsive-nav-link :href="route('projects.index')" :active="request()->routeIs('projects.*')">
+                {{ __('Projects') }}
+            </x-responsive-nav-link>
+
+            <x-responsive-nav-link :href="route('my-tasks')" :active="request()->routeIs('my-tasks')">
+                {{ __('My Tasks') }}
             </x-responsive-nav-link>
         </div>
 
